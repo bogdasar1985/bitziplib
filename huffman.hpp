1fd8ff6aa91f60e9b266359eb26b39a42d3e1d38
bogdasar1985 <bogdasar1985@gmail.com>
2021-07-06 18:19:32 +0500

    Delete comment code

--- huffman.hpp
+++ huffman.hpp
@@ -3,25 +3,6 @@
 #include "priority_queue.hpp"
 #include "bitset.hpp"
 #include <stdio.h>
-/**
-class huffman
-{
-public:
-    huffman();
-    ~huffman();
-    void add_symbol(char sym);
-    void build_tree();
-    bitset get_code(char sym) const;
-    tree_node find_node(char sym) const;
-    size_t pq_size() const;
-private:
-    static void free_tree(tree_node* root);
-    static void find_node(tree_node* root, char sym, tree_node** res);
-    void set_parent_nodes(tree_node* root);
-    priority_queue pq;
-    tree_node* root;
-};
-*/
 namespace huffman
 {
     struct huffman
